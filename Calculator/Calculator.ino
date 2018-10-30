71efda9168c014ddcc3a3bdef6401224d69be01e
Feifan Qiao <qiaofeifan@gmail.com>
2018-10-29 21:31:51 -0700

    Added comments and finalized the program

--- Calculator/Calculator.ino
+++ Calculator/Calculator.ino
@@ -1,19 +1,30 @@
+/*
+ * EE 299 Lab 2 Master Code
+ * 
+ * Feifan Qiao, Mitchell Szeto, Bert Zhao
+ * 
+ * This is the master version. It asks user for a simple mathmatic operation
+ * and sends the result to the slave device
+ * 
+ * Last modified: 10/29/2018
+ */
+
 #include <Wire.h>
 
-const int LINE_FEED = 10; // ascii code for line feed
-const int SPACE = 32;
+const int LINE_FEED = 10;     // ascii code for line feed
+const int SPACE = 32;         // ascii code for space
 
-int incomingByte = 0;   // for incoming serial data
+int incomingByte = 0;         // for incoming serial data
 
 char data = 0;
 char prevData = 0;
-bool firstNegative = false;
-bool secondNegative = false;
-int firstNum = 0;
-int secondNum = 0;
-bool onFirst = true;  // initialized to be on the first number
-char operation = 0;
-bool clearScreen = false;
+bool firstNegative = false;   // checks if the first number is negative
+bool secondNegative = false;  // checks if the second number is negative
+int firstNum = 0;             // stores the value of the first number
+int secondNum = 0;            // stores the value of the second number
+bool onFirst = true;          // checks if current step is on the first number
+char operation = 0;           // stores the operator
+bool clearScreen = false;     // checks if it's time to clear screen
 
 void setup() {
   Serial.begin(9600);     // opens serial port, sets data rate to 9600 bps
@@ -31,11 +42,10 @@ void loop() {
         clearScreen = false;
       }
       
-      // lcd.print(data);
       sendData(data);
       
-      if (onFirst) {
-        if (prevData == 0 && data == '-') {
+      if (onFirst) { 
+        if (prevData == 0 && data == '-') {  // checks if the first input is negative sign
           firstNegative = true;
         } else if (!((prevData != '+' || prevData != '-' || prevData != '*' || prevData != '/') &&
             (data == '+' || data == '-' || data == '*' || data == '/'))) {
@@ -64,7 +74,7 @@ void loop() {
   }
 }
 
-// prints the final result
+// calculates the final result and calls sendDataInt() function
 void calculate() {
   if (firstNegative) {
     firstNum *= -1;
@@ -89,6 +99,7 @@ void calculate() {
   }
 }
 
+// resets all the variable values
 void reset() {
   data = 0;
   prevData = 0;
